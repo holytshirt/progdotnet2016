33bfd56a562fb8216f5e98cdd948048ef941ab7d
Toby Henderson <hendersont@gmail.com>
2016-06-21 17:59:26 +0100

    Basic full code

---
+++ console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+using Language;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = new Localise().Translate(args[0]);
+            var jsonText = JsonConvert.SerializeObject(new { Text = text });
+            Console.WriteLine(jsonText);
+        }
+    }
+}
