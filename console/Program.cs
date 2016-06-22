03ca09f77039542258370d09c66b0daa2a6256f4
Toby Henderson <hendersont@gmail.com>
2016-06-22 11:51:21 +0100

    Added commandline utils

--- console/Program.cs
+++ console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Language;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace ConsoleApplication
 {
@@ -8,7 +9,35 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var text = new Localise().Translate(args[0]);
+            var app = new CommandLineApplication
+            {
+                Name = "Translate Hello World",
+                FullName = ".NET Core Translate Hello World",
+                Description = "Translate hello world based on alpha 2 code",
+            };
+            app.HelpOption("-h|--help");
+            var alphaTwoCodeOption = app.Option("--alphaTwoCode|-a", "Alpha Two Code", CommandOptionType.SingleValue);
+
+            app.OnExecute(() => 
+            {
+                var alphaTwoCode = alphaTwoCodeOption.Value();
+                if (alphaTwoCode == null)
+                {
+                    app.ShowHelp();
+                    return 2;
+                }
+
+                GetTranslated(alphaTwoCode);
+
+                return 0;
+            });
+
+            app.Execute(args);
+        }
+
+        private static void GetTranslated(string alphaTwoCode)
+        {
+            var text = new Localise().Translate(alphaTwoCode);
             var jsonText = JsonConvert.SerializeObject(new { Text = text });
             Console.WriteLine(jsonText);
         }
